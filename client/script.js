7ea2ee459f0bbdcef6adac9baa37283f76de2293
Andy Sterkowitz <andy@andysterkowitz.com>
2019-10-24 21:25:36 -0400

    Adds spinner to table when loading initially

--- client/script.js
+++ client/script.js
@@ -1,8 +1,37 @@
+(function() {
+
+  const table = document.getElementById("prospect-table");
+  const columnHeaderArray = ["Name", "Email"];
+  createTableHeader(columnHeaderArray);
+  
+  const tbodyEl = document.createElement("tbody");
+  table.appendChild(tbodyEl);
+  
+  const emptyDataRow = document.createElement("TR");
+  const emptyDataCell = document.createElement("TD");
+  emptyDataCell.className = 'align-middle';
+  emptyDataCell.setAttribute("colspan", 3);
+  const spinnerDiv = document.createElement('DIV');
+  spinnerDiv.className = 'spinner-border spinner-border-sm mr-2';
+  const spinnerSpan = document.createElement('SPAN');
+  spinnerSpan.className = 'sr-only';
+  spinnerSpan.textContent = 'Loading...';
+  spinnerDiv.appendChild(spinnerSpan);
+  const textSpan = document.createElement('SPAN');
+  textSpan.textContent = 'Loading...';
+  emptyDataCell.appendChild(spinnerDiv);
+  emptyDataCell.appendChild(textSpan);
+  emptyDataRow.appendChild(emptyDataCell);
+  tbodyEl.appendChild(emptyDataRow);
+  
+  //setTableLoading();
+
+})();
+
 fetchProspects().then(prospects => {
-  createProspectTable(prospects);
+  recreateProspectTable(prospects);
 });
 
 function fetchProspects() {
-  return fetch("api/prospects")
-    .then(res => res.json());
+  return fetch("api/prospects").then(res => res.json());
 }
